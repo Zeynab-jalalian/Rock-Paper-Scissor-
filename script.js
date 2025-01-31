e959bd2beaac9f4ec16586dc8fe69545a132bc1f
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-31 20:45:33 +0330

    DOM js

---
+++ script.js
@@ -0,0 +1,5 @@
+const game=document.querySelector('.container'),
+user=document.querySelector('.user img'),
+cpu=document.querySelector('.cpu img'),
+resultText=document.querySelector('.resultText'),
+options=document.querySelector('.option');
