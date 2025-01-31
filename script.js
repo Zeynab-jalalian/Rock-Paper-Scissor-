772ca32eb22d900ee7978981c0298df3491bc046
zeynab jalalian <zjalalianweb@gmail.com>
2025-01-31 20:57:48 +0330

    active and start js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-const game=document.querySelector('.container'),
-user=document.querySelector('.user img'),
-cpu=document.querySelector('.cpu img'),
-resultText=document.querySelector('.resultText'),
-options=document.querySelectorAll('.option');
+const game = document.querySelector('.container'),
+    user = document.querySelector('.user img'),
+    cpu = document.querySelector('.cpu img'),
+    resultText = document.querySelector('.resultText'),
+    options = document.querySelectorAll('.option');
 
+options.forEach((img, index) => {
+    img.addEventListener('click', (e) => {
+    img.classList.add('active');
+    options.forEach((img2,index2)=>{
+        if(index!==index2){
+            img2.classList.remove('active');
+        }
+    })
+    
+   user.src=cpu.src="img/rock.png";
+   game.classList.add('start');
+    
+
+    })
+
+
+})
